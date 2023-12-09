83e60fcca6a75ad0e454bd17dc63150b431880d9
ngedot <152137599+ngedot@users.noreply.github.com>
2023-12-10 02:36:29 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/ngempeng/botol/master/limit/menu.zip
+    wget https://raw.githubusercontent.com/ngempeng/jempol/master/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
