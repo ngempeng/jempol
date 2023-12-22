47e3bb5ab76bc7e08f1b2e483b1dcddabcc14a69
ngedot <152137599+ngedot@users.noreply.github.com>
2023-12-22 14:25:26 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -243,8 +243,8 @@ elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"/
         #gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
     #echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
         #http://haproxy.debian.net buster-backports-1.8 main \
-        >/etc/apt/sources.list.d/haproxy.list
-    apt update
+        #>/etc/apt/sources.list.d/haproxy.list
+    apt update -y
     apt install haproxy -y
 else
     echo -e " Your OS Is Not Supported ($(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g') )"
